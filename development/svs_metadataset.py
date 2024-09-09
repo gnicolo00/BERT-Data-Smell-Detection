ecdf55e483b0776fbeb1e454085d81670cdbd5b8
gnicolo00 <ngallotta00@gmail.com>
2024-09-09 21:06:52 +0200

    Created meta-dataset for Splitted Value Smells

---
+++ development/svs_metadataset.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import glob
+import os
+
+# Raccolta di tutti i dataset nella cartella "sub-datasets/svs"
+csv_files = glob.glob(os.path.join("..", "datasets",  "sub-datasets", "svs", "*.csv"))
+datasets = []
+for file in csv_files:
+    df = pd.read_csv(file)
+    datasets.append(df)
+
+
+# Trasformazione di un singolo dataset in meta-dataset mantenendo l'ordine delle righe
+def transform_to_meta(dataset, id_dataset):
+    sub_meta_dataset_rows = []
+    num_columns = len(dataset.columns)
+
+    for index in range(len(dataset)):
+        for i, column in enumerate(dataset.columns):
+            if i == num_columns - 1: # L'ultima colonna di ogni dataset è smelly, tutte le altre no
+                smell_value = 0
+            else:
+                smell_value = 1
+
+            sub_meta_dataset_rows.append({
+                'ID Dataset': id_dataset,
+                'Nome Feature': column,
+                'Valore': dataset.at[index, column],
+                'Smell': smell_value
+            })
+
+    return pd.DataFrame(sub_meta_dataset_rows)
+
+
+sub_meta_datasets = []
+for i, dataset in enumerate(datasets):
+    sub_meta_datasets.append(transform_to_meta(dataset, i+1))
+
+# Unione dei meta-dataset in uno solo
+svs_meta_dataset = pd.concat(sub_meta_datasets, ignore_index=True)
+svs_meta_dataset.to_csv(os.path.join("..", "datasets",  "meta-datasets", "svs_metadataset.csv"), index=False)
