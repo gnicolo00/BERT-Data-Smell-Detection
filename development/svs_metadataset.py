f5d61ad78dff4270d1b50fa721bff3e0a3ed7910
gnicolo00 <ngallotta00@gmail.com>
2024-09-10 02:06:32 +0200

    Fixed Data Visualization for the SVS meta-dataset

--- development/svs_metadataset.py
+++ development/svs_metadataset.py
@@ -18,7 +18,7 @@ def transform_to_meta(dataset, id_dataset):
 
     for index in range(len(dataset)):
         for i, column in enumerate(dataset.columns):
-            if i == num_columns - 1: # L'ultima colonna di ogni dataset è smelly, tutte le altre no
+            if i == num_columns - 1: # Tutte le colonne di ogni dataset sono smelly, eccetto l'ultima
                 smell_value = 0
             else:
                 smell_value = 1
@@ -51,17 +51,17 @@ print(null_count)
 # Creazione e salvataggio di un pie plot
 fig, ax = plt.subplots(figsize=(7, 6))
 fig.subplots_adjust(left=0, right=1, top=1, bottom=0.1)
-plt.pie(svs_meta_dataset['Smell'].value_counts(), labels=['Non-Smelly', 'Smelly'], colors=["#CCCCCC", "#28A745"],
-        explode=(0, 0.015), autopct="%0.2f", startangle=90, textprops={'fontsize': 11})
+plt.pie(svs_meta_dataset['Smell'].value_counts(), labels=['Smelly', 'Non-Smelly'], colors=['#28A745', '#CCCCCC'],
+        explode=(0, 0.015), autopct='%0.2f', startangle=90, textprops={'fontsize': 11})
 plt.savefig(os.path.join("..", "plots", "svs-balancing-pie.png"), format="png")
 
 # Creazione e salvataggio di un bar plot
 smell_counts = svs_meta_dataset['Smell'].value_counts()
 plt.figure(figsize=(8, 6))
-plt.bar(smell_counts.index, smell_counts.values, color=['#CCCCCC', '#28A745'], edgecolor='black', linewidth=1)
+plt.bar(smell_counts.index, smell_counts.values, color=['#28A745', '#CCCCCC'], edgecolor='black', linewidth=1)
 plt.title('Distribuzione dei dati Smelly vs Non-Smelly', fontsize=16)
 plt.xlabel('Splitted Value Smell', fontsize=14)
 plt.ylabel('Numero di Istanze', fontsize=14)
-plt.xticks([0, 1], ['Smelly', 'Non-Smelly'])
+plt.xticks([0, 1], ['Non-Smelly', 'Smelly'])
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.savefig(os.path.join("..", "plots", "svs-balancing-bar.png"), format="png")
